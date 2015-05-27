fe79d9e999913e5dc9c78e1acacc8ae396f57e25
Harris Novick <harrisdavidnovick@gmail.com>
2015-05-26 20:19:16 -0400

    Add retry options to CreateDocument worker.

--- app/workers/create_document_worker.rb
+++ app/workers/create_document_worker.rb
@@ -1,5 +1,10 @@
 class CreateDocumentWorker
   include Sidekiq::Worker
+  sidekiq_options retry: 2
+
+  sidekiq_retry_in do |count|
+    30 * (count + 1) # (i.e. 30, 60, 90, 120)
+  end
 
   def perform(url:, content_type:, classification:)
     CreateDocument.run!(url: url, content_type: content_type, classification: classification)
